aec51a6b023f3acbe9609e2a11098c2f78be0680
liujiarui <liujiarui@ship56.net>
2019-03-29 17:28:30 +0800

    新增StatePager,还未完善，先存档

---
+++ app/src/main/java/cn/leo/adapter/StatePager.java
@@ -0,0 +1,118 @@
+package cn.leo.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author : Jarry Leo
+ * @date : 2019/3/29 14:01
+ */
+public class StatePager {
+    private Context mContext;
+    private View mView;
+    private View mTarget;
+    private View mReplace;
+    private int mLoadingId = View.NO_ID;
+    private int mEmptyId = View.NO_ID;
+    private int mErrorId = View.NO_ID;
+
+    private StatePager(View view) {
+        mView = view;
+        mContext = view.getContext();
+    }
+
+    public static StatePager getInstance(@NonNull View view) {
+        return new StatePager(view);
+    }
+
+    public static StatePager getInstance(@NonNull Activity activity) {
+        return new StatePager(activity.getWindow().getDecorView());
+    }
+
+    public static StatePager getInstance(@NonNull Fragment fragment) {
+        if (fragment.getView() == null) {
+            throw new NullPointerException("fragment not attach");
+        }
+        return new StatePager(fragment.getView());
+    }
+
+    public static StatePager getInstance(@NonNull android.app.Fragment fragment) {
+        if (fragment.getView() == null) {
+            throw new NullPointerException("fragment not attach");
+        }
+        return new StatePager(fragment.getView());
+    }
+
+    /**
+     * 替代view位置展示不同状态page
+     * 如果view 的父view 是
+     */
+    public StatePager successViewId(@IdRes int id) {
+        mTarget = getViewById(id);
+        return this;
+    }
+
+    public StatePager loadingViewId(@IdRes int id) {
+        mLoadingId = id;
+        return this;
+    }
+
+    public StatePager emptyViewId(@IdRes int id) {
+        mEmptyId = id;
+        return this;
+    }
+
+    public StatePager errorViewId(@IdRes int id) {
+        mErrorId = id;
+        return this;
+    }
+
+    public StatePager setRetryButtonId(@IdRes int id) {
+
+        return this;
+    }
+
+    public StatePager setRetryClickListener(View.OnClickListener listener) {
+
+        return this;
+    }
+
+    /**
+     * 根据id查找view
+     */
+    private View getViewById(@IdRes int viewId) {
+        View view = mView.findViewById(viewId);
+        if (view == null) {
+            String entryName = mView.getResources().getResourceEntryName(viewId);
+            throw new NullPointerException("id: R.id." + entryName + " can not find in this view!");
+        }
+        return view;
+    }
+
+
+    private View getViewById(@NonNull View view, @IdRes int viewId) {
+        return view.findViewById(viewId);
+    }
+
+    private void repalceView(int layoutRes) {
+        if (mReplace != null) {
+            int id = mReplace.getId();
+            if (id == layoutRes) {
+                return;
+            }
+        }
+        ViewGroup parent = (ViewGroup) mView.getParent();
+        parent.removeView(mReplace);
+        View inflate = LayoutInflater.from(mContext).inflate(layoutRes, null);
+        inflate.setLayoutParams(mView.getLayoutParams());
+        inflate.setId(layoutRes);
+        mReplace = inflate;
+
+    }
+}
